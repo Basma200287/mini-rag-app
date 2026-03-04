9d9adf7516b30fc7f9d54fff92ca286d79aed308
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-03-04 16:43:42 +0000

    update requirements

--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 
-base_router = APIRouter()
- 
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"],
+)
+
 @base_router.get("/")
 def welcome():
     return {
-        "message": "Hello bestie"
+        "message": "Hello World"
     }
