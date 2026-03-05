8c92e0b8401abccc70a3b853e98785b28836ec25
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-03-05 10:42:59 +0000

    update main et base

--- routes/base.py
+++ routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+import os
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
@@ -7,6 +7,11 @@ base_router = APIRouter(
 
 @base_router.get("/")
 def welcome():
+APP_NAME="mini-RAG"
+    app_name = os.getenc('APP_NAME')
+    app_versiom = os.getenv('APP_VERSION')
+
     return {
-        "message": "Hello World"
+        'app_name': app_name,
+        'app_version' : app_version 
     }
