7087f8c44726d80f1975f8e24caaf4e2d7b01532
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-03-10 09:51:05 +0000

    updates

--- SRC/helpers/config.py
+++ SRC/helpers/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION: str
-    OPENAI_API_KEY: str
+    OPENAI_API_KEY: str 
 
     class Config:
         env_file=".env"
--- SRC/routes/base.py
+++ SRC/routes/base.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from helpers import get_settings
+from fastapi import APIRouter, Depends
+from helpers.config import get_settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,7 +7,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
+async def welcome(app_settings = Depends(get_settings)):
     app_settings = get_settings()
      
     app_name = app_settings.APP_NAME
