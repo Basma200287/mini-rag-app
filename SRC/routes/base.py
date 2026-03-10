7a38ed494d8f6250d2512c695d42770d476f815e
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-03-10 23:41:21 +0000

    update base

--- SRC/routes/base.py
+++ SRC/routes/base.py
@@ -8,7 +8,6 @@ base_router = APIRouter(
 
 @base_router.get("/")
 async def welcome(app_settings = Depends(get_settings)):
-    app_settings = get_settings()
      
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
