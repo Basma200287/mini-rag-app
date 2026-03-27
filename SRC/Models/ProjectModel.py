df8271a28e7e941b0d25043351ad32199bd5a037
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-03-27 11:37:19 +0000

    updates

--- SRC/Models/ProjectModel.py
+++ SRC/Models/ProjectModel.py
@@ -7,6 +7,25 @@ class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+        
+    @classmethod
+    async def create_instance(cls,db_client: object):
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
+    
+    async def init_collection(self):
+        all_collections= await self.db_client.list_collection_names()
+        if DataBaseEnum.COLLECTION_PROJECT_NAME.value not in all_collections:
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+            indexes = Project.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index["key"],
+                    name=index["name"],
+                    unique=index.get("unique", False)
+                )
+            
 
     async def create_project(self, project:Project): 
 
@@ -16,6 +35,9 @@ class ProjectModel(BaseDataModel):
         return project
 
     async def get_project_or_create_one(self, project_id: str):
+        
+        if not project_id or project_id.strip() == "":
+            raise ValueError("project_id must be non-empty and alphanumeric")
 
         record = await self.collection.find_one({
             "project_id":project_id
@@ -41,7 +63,7 @@ class ProjectModel(BaseDataModel):
             total_pages += 1
 
         cursor = self.collection.find().skip((page-1)*page_size).limit(page_size)
-        Projects = []
+        projects = []
         async for document in cursor:
             projects.append(
                 Project(**document)
--- SRC/routes/data.py
+++ SRC/routes/data.py
@@ -26,7 +26,7 @@ async def upload_data(request: Request, project_id:str,file:UploadFile,
                       app_settings:Settings = Depends(get_settings)):
 
 
-                      project_model= ProjectModel(
+                      project_model= await ProjectModel.create_instance(
                         db_client= request.app.db_client
                       )
                       project = await project_model.get_project_or_create_one(
@@ -93,7 +93,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model= ProjectModel(
+    project_model= await ProjectModel.create_instance(
         db_client= request.app.db_client
         )
     project = await project_model.get_project_or_create_one(
@@ -126,7 +126,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
         for i, chunk in enumerate(file_chunks)
     ]
     
-    chunk_model = ChunkModel(
+    chunk_model = await ChunkModel.create_instance(
         db_client= request.app.db_client
     )
 
