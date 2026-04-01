99e0e7fade18759ccd99523f3e6b97c9502b1b42
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-04-01 10:07:27 +0000

    updates

--- SRC/stores/vectordb/providers/QdrantDBProvider.py
+++ SRC/stores/vectordb/providers/QdrantDBProvider.py
@@ -2,6 +2,7 @@ from qdrant_client import models, QdrantClient
 from vectordb.VectorDBInterface import VectorDBInterface 
 from vectordb.VectorDBEnums import DistanceMethodEnums 
 import logging 
+from typing import List
 
 class QdrantDBProvider(VectorDBInterface):
     
@@ -11,12 +12,12 @@ class QdrantDBProvider(VectorDBInterface):
         self.db_path = db_path
         self.distance_method = distance_method
 
-    if distance_method== DistanceMethodEnums.COSINE.value:
-        self.distance_method = models.Distance.COSINE
-    elif distance_method == DistanceMethodEnums.DOT.value:
-        self.distance_method = models.Distance.DOT
+        if distance_method== DistanceMethodEnums.COSINE.value:
+            self.distance_method = models.Distance.COSINE
+        elif distance_method == DistanceMethodEnums.DOT.value:
+            self.distance_method = models.Distance.DOT
 
-    self.logger= logging.getLogger(__name__)
+        self.logger= logging.getLogger(__name__)
 
     def connect(self):
         self.client = QdrantClient(path=self.db_path)
@@ -30,7 +31,7 @@ class QdrantDBProvider(VectorDBInterface):
     def list_all_collections(self) -> List:
         return self.client.get_collections()
     
-    def get_collections_info(self)-> dict:
+    def get_collections_info(self, collection_name: str)-> dict:
         return self.client.get_collection(collection_name=collection_name)
     
     def delete_collection(self, collection_name: str):
