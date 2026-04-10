f96cbaeb643cc31243cc7238edf7960687edb646
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-04-10 04:01:10 +0000

    update schemes

---
+++ SRC/Models/db_schemes/minirag/schemes/__init_.py
@@ -0,0 +1,4 @@
+from Models.db_schemes.minirag.shemes.minirag_base import SQLAlchemyBase
+from Models.db_schemes.minirag.shemes.asset import Asset
+from Models.db_schemes.minirag.shemes.project import Project
+from Models.db_schemes.minirag.shemes.datachunk import DataChunk, RetrievedDocument
