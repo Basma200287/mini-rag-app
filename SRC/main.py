f0938148a90c7ca86847b811f7868269b6479576
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-04-11 17:35:52 +0000

    updates

--- SRC/main.py
+++ SRC/main.py
@@ -19,9 +19,8 @@ async def startup_span():
         app.db_engine, class_=AsyncSession, expire_on_commit=False
     )
 
-
     llm_provider_factory = LLMProviderFactory(settings)
-    vectordb_provider_factory = VectorDBProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(config=settings, db_client=app.db_client)
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
@@ -36,7 +35,7 @@ async def startup_span():
     app.vectordb_client = vectordb_provider_factory.create(
         provider=settings.VECTOR_DB_BACKEND
     )
-    app.vectordb_client.connect()
+    await app.vectordb_client.connect()
 
     app.template_parser = TemplateParser(
         language=settings.PRIMARY_LANG,
@@ -45,7 +44,7 @@ async def startup_span():
 
 
 async def shutdown_span():
-    await app.db_engine.dispose()
+    app.db_engine.dispose()
     await app.vectordb_client.disconnect()
 
 app.on_event("startup")(startup_span)
