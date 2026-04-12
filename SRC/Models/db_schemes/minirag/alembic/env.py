308645c8b8f845625557a78640e271f7af8d2267
Basma Bouazizi <basmabouazizi2002@gmail.com>
2026-04-12 20:47:02 +0000

    updates

--- SRC/Models/db_schemes/minirag/alembic/env.py
+++ SRC/Models/db_schemes/minirag/alembic/env.py
@@ -2,11 +2,13 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from schemes.minirag_base import SQLAlchemyBase
+from schemes.minirag_base import SQLAlchemyBase
+from schemes.asset import Asset
+from schemes.project import Project
+from schemes.datachunk import DataChunk
 
 from alembic import context
-
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,8 +23,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLAlchemyBase.metadata 
-
+target_metadata = SQLAlchemyBase.metadata
+print(SQLAlchemyBase.metadata.tables.keys())
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
